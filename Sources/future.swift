4abae20f85f0814da8454f107786e4d4298a28ab
Lance Stammer <las36@dynip-192-168-46-63.dhcp.cs.uwf.edu>
2017-02-23 15:02:50 -0600

    corrected calculations for consistency

--- Sources/future.swift
+++ Sources/future.swift
@@ -1,26 +1,27 @@
 import Foundation
-import Darwin
 
 public struct future_value {
     public let p: Double
     public let r: Double
     public let t: Double
     
-    public init(principal: Double, apr: Double, years: Double){
+    public init(_ principal: Double, _ apr: Double, _ years: Double){
         self.p = principal
-        self.r = apr
+        self.r = apr / 100
         self.t = years
     }
     
-    func print_future(){
+    public func print_future(){
         let res = p * pow(1+r,Double(t)) //future value calcuation
         let resString = round(res*100)/100 //FV rounded to two decimals
         
         //print out for the future value calculations
+        print(" \n ") //added blank lines for a cleaner printout
         print("Future Value Calculation Results")
         print("Principal Deposit: $\(p)")
         print("APR:  \(r*100)%")
         print("Number of Years:  \(t)")
         print("Future Value Single Sum:  $\(resString)")
+        print(" \n ") //added blank lines for a cleaner printout
     }
 }
